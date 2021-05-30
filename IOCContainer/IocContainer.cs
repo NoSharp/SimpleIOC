786313b3bfcc622dd4b5d1fe5188244dcbcb4516
nosharp <nosharp1@gmail.com>
2021-05-30 08:09:39 +0100

    feat: initial commit.

---
+++ IOCContainer/IocContainer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace IOCContainer{
+
+    /// <summary>
+    /// A simple IoC container that allows for constructor dependency Injection.
+    /// </summary>
+    public class IocContainer
+    {
+
+        private readonly Dictionary<Type, object> _singletons;
+
+        private static IocContainer _containerInstance;
+
+        public static IocContainer Instance
+        {
+            get { return _containerInstance ??= new IocContainer(); }
+        }
+
+        public IocContainer()
+        {
+            _singletons = new Dictionary<Type, object>();
+        }
+
+        private ConstructorInfo ResolveConstructor(Type type)
+        {
+            try
+            {
+                return type.GetConstructors().FirstOrDefault();
+            }
+            catch (MissingMethodException _)
+            {
+                Console.WriteLine("No construcotr!");
+                return null;
+            }
+        }
+
+        private T SafelyInstantiateObject<T>(params object[] args)
+        {
+            if (args.Length == 0)
+            {
+                return (T) Activator.CreateInstance(typeof(T));
+            }
+            
+            return (T) Activator.CreateInstance(typeof(T), args.ToArray());
+            
+        }
+
+        private T ConstructObject<T>(params object[] extraArguments)
+        {
+            var type = typeof(T);
+            List<object> arguments = new List<object>();
+            ConstructorInfo constructor = ResolveConstructor(type);
+            
+            if (constructor == null)
+            {
+                return SafelyInstantiateObject<T>(arguments);
+            }
+
+            foreach (var parameterInfo in constructor.GetParameters())
+            {
+                var paramType = parameterInfo.ParameterType;
+                if (_singletons.ContainsKey(paramType))
+                    arguments.Add(this._singletons[paramType]);
+            }
+            
+            foreach (var arg in extraArguments)
+                arguments.Add(arg);
+            
+            return SafelyInstantiateObject<T>(arguments.ToArray());
+        }
+
+        public void Set<T, TV>(params object[] args) where TV : T
+        {
+            Type type = typeof(T);
+            
+            if (_singletons.ContainsKey(type))
+            {
+                throw new Exception("Duplicate singleton found in IOCContainer.");
+            }
+
+            _singletons.Add(type, ConstructObject<TV>(args));
+        }
+
+        public T Get<T>()
+        {
+            return (T)_singletons[typeof(T)];
+        }
+
+    }
+}
